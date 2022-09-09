be435d7efd9bb8bb18a4ea67beaa5ddb50e93e34
fanusi <stephanetrouve@hotmail.com>
2022-09-09 10:27:05 +0200

    Initial Commit

---
+++ Qatar_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Qatar_1
+//
+//  Created by Stéphane Trouvé on 09/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
