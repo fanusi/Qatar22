e0fdc9b326ba5a8b3182f4af6a78f97b551c1baa
fanusi <stephanetrouve@hotmail.com>
2026-06-01 22:14:46 +0200

    Backup voor nieuwe MacOS

---
+++ Qatar_1/Extensions/Support.swift
@@ -0,0 +1,8 @@
+//
+//  Support.swift
+//  Qatar_1
+//
+//  Created by Stéphane Trouvé on 09/09/2022.
+//
+
+import Foundation
---
+++ TabPageScrollViewController/Model/PageModel.swift
@@ -0,0 +1,8 @@
+//
+//  PageModel.swift
+//  Qatar_1
+//
+//  Created by Stéphane Trouvé on 09/09/2022.
+//
+
+import Foundation
